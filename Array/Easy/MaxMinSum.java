d5e24dbf69dc9638e2de70857319d7cfa4a861dd
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-05-27 22:15:22 +0530

    Update MaxMinSum.java

--- Array/Easy/MaxMinSum.java
+++ Array/Easy/MaxMinSum.java
@@ -1,7 +1,16 @@
- 
-class Solution
+ import java.util.Scanner;
+class MaxMinSum
 { 
-    public static int MaxMinSum(int A[], int N) 
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+     int arr[] = new int[n];
+     for(int i=0;i<n;i++){
+      arr[i]=sc.nextInt();
+     }
+     System.out.println(maxMinSum(arr,n));
+    }
+    public static int maxMinSum(int A[], int N) 
     {
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;

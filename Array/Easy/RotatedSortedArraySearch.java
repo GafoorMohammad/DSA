73c591d44003a26d08981ee7e918449fb1dec0fa
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-05-27 22:49:06 +0530

    Create RotatedSortedArraySearch.java

---
+++ Array/Easy/RotatedSortedArraySearch.java
@@ -0,0 +1,41 @@
+class RotatedSortedArraySearch {
+    public static int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target&&target<nums[mid]){
+                    right = mid-1;
+                }
+                else{
+                    left = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target&&target<=nums[right]){
+                    left=mid+1;
+                }
+                else{
+                    right = mid-1;
+                }
+            }
+            
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr [] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int target = sc.nextInt();
+        System.out.print(search(arr,target));
+    }
+}
